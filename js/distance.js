408b52f7205e99559eb5e95cfc61459677b31d13
tarakuds <taradebby@gmail.com>
2020-07-03 08:53:48 +0100

    demo upload

---
+++ js/distance.js
@@ -0,0 +1,6 @@
+//   TO CALCULATE DISTANCE
+
+origins = (Bobcaygeon + ON) | (24 + Sussex + Drive + Ottawa + ON);
+destination = (Bobcaygeon + ON) | (24 + Sussex + Drive + Ottawa + ON);
+
+// link for
---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(".homepage-banner").vegas({
+  // transitionDuration: 7000,
+  timer: true,
+  slides: [
+    {
+      src: "img/bg1.jpg",
+    },
+    {
+      src: "img/bg2.jpg",
+    },
+    {
+      src: "img/bg3.jpg",
+    },
+  ],
+  animation: "kenburnsLeft",
+});
